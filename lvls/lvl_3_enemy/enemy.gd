024b1d8d70f289b88070555fef9aa332f23760a7
andres <andres@andres-H61ML>
2022-09-15 12:41:36 -0300

    Actualizar enemy.gd

--- lvls/lvl_3_enemy/enemy.gd
+++ lvls/lvl_3_enemy/enemy.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
-var target: KinematicBody2D
+var target: KinematicBody2D						#Variable donde guaramos a target
+
 
 func _ready():
 	pass
@@ -9,6 +10,7 @@ func _process(delta):
 	pass
 	
 func _on_Area2D_body_exited(body):
-	# 
+	# Si body esta en el grupo player
 	if body.is_in_group("player"):
+		# asigna body a target
 		target = body
